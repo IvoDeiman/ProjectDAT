2240c4336c2aa86d69185369e17c4f46d44b7e5e
Ivo Deiman <iddeiman@hotmail.com>
2023-06-28 14:45:04 +0200

    Changed AudioChecker.cs to work with Button Input. Press "E" for results (Pretty much re-wrote all your code Nico. Sorry, not sorry)

--- Assets/AudioChecker.cs
+++ Assets/AudioChecker.cs
@@ -5,46 +5,77 @@ using UnityEngine;
 public class AudioChecker : MonoBehaviour
 {
 
-    public List<GameObject> scannedObjects = new List<GameObject>();
     public float rotateAngle = .5f;
 
+    public List<GameObject> scannedObjects = new List<GameObject>();
 
-    private void Update() {
+    // Should be a copy of the scannedObjects list but instead of GameObject data, 
+    // It should have Target data, allowing is to invoke without constant GetComponent Usage.
+    private List<Target> scannedTargets = new List<Target>(); 
+
+
+    private void Update()
+    {
         transform.Rotate(Vector3.up * (rotateAngle * Time.deltaTime));
     }
 
-    private void OnTriggerEnter(Collider other) {
-        // fuck off capsule
-        if (other.gameObject.name == "Capsule") return;
-        if (other.gameObject.GetComponent<Target>() != null) {
-            other.gameObject.GetComponent<Target>().ScannerEnter.Invoke();
-        }
-        // executing unity event on object to start senses
-        /*
-        if (other.gameObject.GetComponent<FocusEffect>() != null) {
-            other.gameObject.GetComponent<FocusEffect>().startEnhance();
-        }*/
+    private void OnTriggerEnter(Collider other)
+    {
+        // Pretty sure this check is not needed but I'm still putting it in here for safety.
+        // Also fuck off capsule.
+        if (scannedObjects.Contains(other.gameObject) || other.gameObject.name == "Capsule") return;
+
+        // Removed redundant calls of GetComponent to improve performance.
+        Target otherTarget = other.gameObject.GetComponent<Target>();
+
+        // Stop any object not having the Target Component.
+        if (otherTarget == null) return;
+
+        // Scanned Objects now should have only Objects with the Target Class.
         scannedObjects.Add(other.gameObject);
-
+        scannedTargets.Add(otherTarget);
     }
-    private void OnTriggerExit(Collider other) {
-        if (other.gameObject.name == "Capsule") return;
-        /*
-        if (other.gameObject.GetComponent<FocusEffect>() != null) {
-            other.gameObject.GetComponent<FocusEffect>().startNormal();
-        }*/
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (!scannedObjects.Contains(other.gameObject)) return;
 
-
-        foreach (GameObject gameObject in scannedObjects) {
-            if (gameObject.gameObject == other.gameObject) {
-                if (other.gameObject.GetComponent<Target>() != null) {
-                    other.gameObject.GetComponent<Target>().ScannerExit.Invoke();
-                }
-                scannedObjects.Remove(gameObject);
-                return;
+        //When button is pressed, Invoke ScannerEnter.
+        //Must be GetKey to account for any objects entering the field.
+        if (Input.GetKey(KeyCode.E))
+            foreach (Target target in scannedTargets)
+            {
+                //Only invoke ScannerEnter if target has not invoked ScannerEnter yet.
+                if (!target.IsInvoking("ScannerEnter"))
+                    target.ScannerEnter.Invoke();
             }
-        }
+        //When button is let go, Invoke ScannerExit.
+        else if (Input.GetKeyUp(KeyCode.E))
+            foreach (Target target in scannedTargets)
+            {
+                // Only invoke ScannerExit if target is invoking ScannerEnter.
+                // if (target.IsInvoking("ScannerEnter"))
+                    target.ScannerExit.Invoke();
+            }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        Target otherTarget = other.gameObject.GetComponent<Target>();
+
+        if (!scannedObjects.Contains(other.gameObject) || otherTarget == null) return;
+
+        // In case the target is leaving the trigger while still invoking ScannerEnter,
+        // Switch it back to ScannerExit.
+        if (otherTarget.IsInvoking("ScannerEnter"))
+            otherTarget.ScannerExit.Invoke();
+
+        // Remove other gameObject from the scannedObjects List.
+        if (scannedObjects.Contains(other.gameObject))
+            scannedObjects.Remove(other.gameObject);
+
+        // Remove other Target from the scannedTargets List.
+        if (scannedTargets.Contains(otherTarget))
+            scannedTargets.Remove(otherTarget);
     }
 }
