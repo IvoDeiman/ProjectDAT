3002e637cc7a6fc97351725b7b0f669c4b84d967
Ivo Deiman <iddeiman@hotmail.com>
2023-07-01 16:41:28 +0200

    Current progress. Sitll unstable.

---
+++ Assets/AudioScriptPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class AudioScriptPlayer : MonoBehaviour
+{
+    [SerializeField] private VoiceOver[] voScenes;
+    [SerializeField] private AudioSource _audio;
+
+    public void StartSceneImmediate(int sceneNumber)
+    {
+        _audio.clip = voScenes[sceneNumber].voiceLines[0];
+        _audio.Play();
+    }
+
+    public void StartSceneDelayed(int sceneNumber, float delay)
+    {
+        _audio.clip = voScenes[sceneNumber].voiceLines[0];
+        _audio.PlayDelayed(delay);
+    }
+}
+
+public struct VoiceOver
+{
+    public AudioClip[] voiceLines;
+}
---
+++ Assets/ThreatDetector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class ThreatDetector : MonoBehaviour
+{
+    [SerializeField][Tooltip("Should exactly match the name of the the tag.")] private string threatTag = "Threat";
+
+    private GameObject threatObject;
+    private Target threatTarget;
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag(threatTag)) return;
+
+        threatObject = other.gameObject;
+        threatTarget = gameObject.GetComponent<Target>();
+    }
+
+    public void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject != threatObject || threatObject == null) return;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (!threatTarget.IsInvoking("ScannerEnter"))
+                threatTarget.ScannerEnter.Invoke();
+        }
+        else
+            threatTarget.ScannerExit.Invoke();
+    }
+
+    public void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject != threatObject || threatObject == null) return;
+
+        threatObject = null;
+        threatTarget = null;
+    }
+}
+
